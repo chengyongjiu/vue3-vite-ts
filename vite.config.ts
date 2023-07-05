bf618da181eabbd764aec02b563fdb0da861bc30
陈永久 <codechenzi@gmail.com>
2023-07-05 11:45:10 +0800

    添加proxy代理

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import * as path from "path";
+import { fileURLToPath, URL } from "node:url";
 import eslintPlugin from "vite-plugin-eslint";
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "./",
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src")
+      "@": fileURLToPath(new URL("./src", import.meta.url))
     }
   },
   plugins: [
@@ -15,5 +15,27 @@ export default defineConfig({
     eslintPlugin({
       include: ["src/**/*.js", "src/**/*.vue", "src/*.js", "src/*.vue"]
     })
-  ]
+  ],
+  server: {
+    /** 是否开启 HTTPS */
+    https: false,
+    /** 设置 host: true 才可以使用 Network 的形式，以 IP 访问项目 */
+    host: true, // host: "0.0.0.0"
+    /** 端口号 */
+    port: 3333,
+    /** 是否自动打开浏览器 */
+    open: false,
+    /** 跨域设置允许 */
+    cors: true,
+    /** 端口被占用时，是否直接退出 */
+    strictPort: false,
+    /** 接口代理 */
+    proxy: {
+      "/api/v1": {
+        target: "http://localhost:5163",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api\/v1/, "")
+      }
+    }
+  }
 });
